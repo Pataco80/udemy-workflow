a997d521c7908a0e99eb492d67907c629e11bacd
Pataco80 <pataco80@gmail.com>
2017-10-29 19:30:19 +0100

    Installed browser-sync and configure server task and testing. End session 9

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
 // VARIABLES
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+// PATHS
+var SOURCEPATHS = {
+  sassSource : 'src/scss/*.scss'
+}
+var APPPATHS = {
+  root :'app/',
+  css : 'app/css',
+  js :  'app/js'
+}
+
 
 
 // TASKS
 
 // Styles
 gulp.task('sass', function(){
-  return gulp.src('src/scss/app.scss')
+  return gulp.src(SOURCEPATHS.sassSource)
     .pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
-    .pipe(gulp.dest('dist/css/app.css'));
+    .pipe(gulp.dest(APPPATHS.css));
 });
 
-
+// Server reload
+gulp.task('serve', ['sass'], function(){
+  browserSync.init([APPPATHS.root + '/*.html', APPPATHS.css + '/*.css', APPPATHS.js + '/*.js'],{
+    server: {
+      baseDir : APPPATHS.root
+    }
+  })
+});
 // Default
-gulp.task('default', ['sass']);
+gulp.task('default', ['serve']);
